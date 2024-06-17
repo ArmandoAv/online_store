3b065b55743ba1374dc459816b8873dddbd6faf4
unknown <ArmandoAV>
2024-06-17 11:09:50 -0600

    Se crean los scripts de la base de datos

---
+++ Base_Datos/CheckQuantity.sql
@@ -0,0 +1,23 @@
+---------------------------------------------------
+------------- FUNCTION CHECK_QUANTITY -------------
+---------------------------------------------------
+
+CREATE OR REPLACE FUNCTION FN_CHECKQUANTITY()
+RETURNS TRIGGER AS $$
+BEGIN
+    IF NEW.QUANTITY <= 0 THEN
+        RAISE EXCEPTION 'El valor en la columna QUANTITY no puede ser negativa o cero';
+    END IF;
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+
+----------------------------------------------------------
+------------- TRIGGER CHECK_QUANTITY_TRIGGER -------------
+----------------------------------------------------------
+
+CREATE TRIGGER TR_CHECKQUANTITY
+	BEFORE INSERT ON ORDERDETAILS
+	FOR EACH ROW
+	EXECUTE FUNCTION FN_CHECKQUANTITY();
